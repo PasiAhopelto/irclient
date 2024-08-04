10e655048e60f14dd16243575d5a21e210a0926c
Pasi Ahopelto <pahopelt@gmail.com>
2024-08-04 08:25:38 +0300

    authenticate
    TODO
    return json from makeAuthPayload
    authenticate and return cookie

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 iRacing client written in Go and maybe htmx.
 
 "Of course it's a toy"
+
+ROUGH PLAN
+- get auth cookie from API
+- safe cache email and sha256
+- first actual query: list season
--- getter/main.go
+++ getter/main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"strings"
+)
+
+const AUTH_URL = ""
+const SEASON_URL = "https://members-ng.iracing.com/data/season/list?season_year=2024&season_quarter=3"
+
+func getCredentials() (string, string) {
+	var email, password string
+	fmt.Print("Email:")
+	fmt.Scan(&email)
+	fmt.Print("Password:")
+	fmt.Scan(&password)
+	return email, password
+}
+
+func makeAuthPayload(email string, password string) map[string]string {
+	var joined = password + strings.ToLower(email)
+	var s256 = sha256.Sum256([]byte(joined))
+	var encoded = base64.StdEncoding.EncodeToString(s256[:])
+	return map[string]string{
+		"email":    email,
+		"password": encoded}
+}
+
 func main() {
+	fmt.Print(makeAuthPayload(getCredentials()))
 }
